11b04e0991e124b6402478e524d445da84b261c3
zichunHU <michael_qm@163.com>
2024-08-04 20:38:27 +0800

    更新了run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# 确保脚本在正确的目录中执行
+cd "$(dirname "$0")"
+
 # 创建构建目录，如果已存在则不会报错
 mkdir -p build
 
 # 进入构建目录
 cd build
 
-# 运行CMake生成构建文件
-cmake ..
+# 运行CMake生成构建文件，指向Factory目录
+cmake ../Factory
 
 # 运行make进行编译
 make
